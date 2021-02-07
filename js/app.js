13e47d485aa4ae0db8859b9fed5b32c09677c977
Akash Selvakumar <akashselvae7@gmail.com>
2021-02-06 22:23:46 -0500

    Deleted Old files

--- js/app.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  .then(reg => console.log('sw registered', reg))
-  .catch(err => console.log('sw reg error', err))
-}
